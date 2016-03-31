224a08b6a7f1361b2a35cf6ec08b141c540184a6
Błażej Białas <bialas.blazej@gmail.com>
2016-03-31 02:40:36 +0200

    third

---
+++ codename_chewbacca/Assets/Scripts/TankMovementOne.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using JetBrains.Annotations;
+
+public class TankMovementOne : MonoBehaviour {
+    public CharacterController tankMove;
+    public Transform bla;
+    public float predkoscPoruszania = 500.0f;
+    public float predkoscrotacji = 70.0f;
+    
+    
+
+    public TankMovementOne(CharacterController nowy, Transform bla)
+    {
+        tankMove = nowy;
+        this.bla = bla;
+    }
+	
+	// Update is called once per frame
+	public void klawiatura () {
+       
+    //float ruchPrzodTyl = Input.GetAxis("Vertical") * predkoscPoruszania;
+	  // float ruchLewoPrawo = Input.GetAxis("Horizontal") * predkoscPoruszania;
+       if (Input.GetKey(KeyCode.W))
+        bla.transform.Translate(Vector3.forward * predkoscPoruszania*Time.deltaTime);
+        if (Input.GetKey("s"))
+        bla.transform.Translate(-Vector3.forward * predkoscPoruszania*Time.deltaTime);
+        if (Input.GetKey("a"))
+        bla.Rotate(-Vector3.up * predkoscrotacji*Time.deltaTime);
+        if (Input.GetKey("d"))
+        bla.Rotate(Vector3.up * predkoscrotacji*Time.deltaTime);
+      
+        
+       // Vector3 ruch = new Vector3(0, 0, ruchPrzodTyl);
+
+     // tankMove.Move(ruch * Time.deltaTime);
+	}
+
+}
