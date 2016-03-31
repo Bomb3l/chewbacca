3dfbadb25c5757dbca3d50f970e3d335b02549db
Błażej Białas <bialas.blazej@gmail.com>
2016-03-31 19:26:11 +0200

    7

--- codename_chewbacca/Assets/Scripts/TankMovementOne.cs
+++ codename_chewbacca/Assets/Scripts/TankMovementOne.cs
@@ -5,8 +5,8 @@ using JetBrains.Annotations;
 public class TankMovementOne {
     public CharacterController tankMove;
     public Transform bla;
-    public float predkoscPoruszania = 500.0f;
-    public float predkoscrotacji = 70.0f;
+    public float predkoscPoruszania = 10.0f;
+    public float predkoscrotacji = 7.0f;
     
     
 
@@ -17,23 +17,26 @@ public class TankMovementOne {
     }
 	
 	// Update is called once per frame
-	public void klawiatura () {
-       
-    //float ruchPrzodTyl = Input.GetAxis("Vertical") * predkoscPoruszania;
-	  // float ruchLewoPrawo = Input.GetAxis("Horizontal") * predkoscPoruszania;
-       if (Input.GetKey(KeyCode.W))
-        bla.transform.Translate(Vector3.forward * predkoscPoruszania*Time.deltaTime);
+	public void klawiatura ()
+	{
+	    Vector3 ruch = new Vector3(0, 0, 10);
+   // float ruchPrzodTyl = Input.GetAxis("Vertical") * predkoscPoruszania;
+	 // float ruchLewoPrawo = Input.GetAxis("Horizontal") * predkoscPoruszania;
+	    if (Input.GetKey(KeyCode.W))
+	        bla.transform.Translate(Vector3.right * predkoscPoruszania*Time.deltaTime);
+	       //bla.transform.position(ruch);
+        
         if (Input.GetKey("s"))
-        bla.transform.Translate(-Vector3.forward * predkoscPoruszania*Time.deltaTime);
+        bla.transform.Translate(-Vector3.right * predkoscPoruszania*Time.deltaTime);
         if (Input.GetKey("a"))
         bla.Rotate(-Vector3.up * predkoscrotacji*Time.deltaTime);
         if (Input.GetKey("d"))
         bla.Rotate(Vector3.up * predkoscrotacji*Time.deltaTime);
       
         
-       // Vector3 ruch = new Vector3(0, 0, ruchPrzodTyl);
+      // Vector3 ruch = new Vector3(ruchLewoPrawo,0, ruchPrzodTyl);
 
-     // tankMove.Move(ruch * Time.deltaTime);
+   //  tankMove.Move(ruch * Time.deltaTime);
 	}
 
 }
